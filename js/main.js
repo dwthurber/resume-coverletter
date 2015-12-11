48d834a0c6da11a71d6cb742a3667600f50c77d7
Derek T <fbfb1694@opayq.com>
2015-12-11 20:11:18 +0000

    updated resume workshop

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    
+// resume "dots" menues
+   $("#one-education").click(function(){
+        $("#one-menu").fadeIn(100);
+   }); 
+   $("#one-close").click(function(){
+        $("#one-menu").fadeOut(100);
+   }); 
+   
+//   resume page toggle
+   $('#toggle-one').addClass("toggle-active");
+   
+   $("#toggle-one").click(function(){
+       $("#resume-one").show();
+       $("#resume-two").hide();
+       $("#toggle-one").addClass('toggle-active');
+       $("#toggle-two").removeClass('toggle-active');
+   });
+   $("#toggle-two").click(function(){
+       $("#resume-two").show();
+       $("#resume-one").hide();
+       $("#toggle-two").addClass('toggle-active');
+       $("#toggle-one").removeClass('toggle-active');
+   });
+});
